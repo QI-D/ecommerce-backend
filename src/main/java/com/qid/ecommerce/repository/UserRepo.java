dcb6fa3b375c5153e4498d2facc0195e0d6afdd9
QI-D <qid14@outlook.com>
2024-12-05 16:23:49 -0800

    add user repo
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/main/java/com/qid/ecommerce/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.qid.ecommerce.repository;
+
+import com.qid.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
