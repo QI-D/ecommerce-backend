b59de3c868b45fcc184057bfa49b42544a94a5d2
QI-D <qid14@outlook.com>
2024-12-05 19:56:44 -0800

    add entity to DTO mapper
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/main/java/com/qid/ecommerce/mapper/EntityDtoMapper.java
@@ -0,0 +1,114 @@
+package com.qid.ecommerce.mapper;
+
+import com.qid.ecommerce.dto.*;
+import com.qid.ecommerce.entity.*;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class EntityDtoMapper {
+
+    // map user entity to user DTO basics
+    public UserDto mapUserToDtoBasic(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        userDto.setEmail(user.getEmail());
+        userDto.setRole(user.getRole().name());
+        userDto.setName(user.getName());
+
+        return userDto;
+    }
+
+    // map address entity to address DTO basics
+    public AddressDto mapAddressToDtoBasic(Address address) {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(address.getId());
+        addressDto.setCity(address.getCity());
+        addressDto.setStreet(address.getStreet());
+        addressDto.setProvince(address.getProvince());
+        addressDto.setCountry(address.getCountry());
+        addressDto.setPostalCode(address.getPostalCode());
+
+        return addressDto;
+    }
+
+    // map category entity to category DTO basics
+    public CategoryDto mapCategoryToDtoBasic(Category category) {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(category.getId());
+        categoryDto.setName(category.getName());
+
+        return categoryDto;
+    }
+
+    // map order item entity to order item DTO basics
+    public OrderItemDto mapOrderItemToDtoBasic(OrderItem orderItem) {
+        OrderItemDto orderItemDto = new OrderItemDto();
+        orderItemDto.setId(orderItem.getId());
+        orderItemDto.setQuantity(orderItem.getQuantity());
+        orderItemDto.setStatus(orderItem.getStatus().name());
+        orderItemDto.setCreatedAt(orderItem.getCreatedAt());
+
+        return orderItemDto;
+    }
+
+    // map product entity to product DTO basics
+    public ProductDto mapProductToDtoBasic(Product product) {
+        ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName(product.getName());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        productDto.setImageUrl(product.getImageUrl());
+
+        return productDto;
+    }
+
+    // map user entity to user DTO plus address
+    public UserDto mapUserToDtoPlusAddress(User user) {
+        UserDto userDto = mapUserToDtoBasic(user);
+        if (user.getAddress() != null) {
+            AddressDto addressDto = mapAddressToDtoBasic(user.getAddress());
+            userDto.setAddress(addressDto);
+        }
+
+        return userDto;
+    }
+
+    // map order item to DTO plus product
+    public OrderItemDto mapOrderItemToDtoPlusProduct(OrderItem orderItem) {
+        OrderItemDto orderItemDto = mapOrderItemToDtoBasic(orderItem);
+        if (orderItem.getProduct() != null) {
+            ProductDto productDto = mapProductToDtoBasic(orderItem.getProduct());
+            orderItemDto.setProduct(productDto);
+        }
+
+        return orderItemDto;
+    }
+
+    // map order item to order item DTO plus product and user
+    public OrderItemDto mapOrderItemToDtoPlusProductAndUser(OrderItem orderItem) {
+        OrderItemDto orderItemDto = mapOrderItemToDtoPlusProduct(orderItem);
+        if (orderItemDto.getUser() != null) {
+            UserDto userDto = mapUserToDtoPlusAddress(orderItem.getUser());
+            orderItemDto.setUser(userDto);
+        }
+
+        return orderItemDto;
+    }
+
+    // mao user entity to user DTO with address and order item history
+    public UserDto mapUserToDtoPlusAddressAndOrderHistory(User user) {
+        UserDto userDto = mapUserToDtoPlusAddress(user);
+        if (user.getOrderItemList() != null && !user.getOrderItemList().isEmpty()) {
+            userDto.setOrderItemList(user.getOrderItemList()
+                    .stream()
+                    .map(this::mapOrderItemToDtoPlusProduct)
+                    .collect(Collectors.toList()));
+        }
+
+        return userDto;
+    }
+}
